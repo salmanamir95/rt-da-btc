f22b8a5e18bb8758b914dda34ba91c948254bb1f
Muhammad Salman Amir <mmsa155j3@gmail.com>
2026-05-30 02:20:02 +0500

    print tweak

--- src/analysis/MeanResult.cpp
+++ src/analysis/MeanResult.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void MeanResult::print() const {
-    std::cout << "[Analytics] Dummy MeanResult -> Time: " << time
+    std::cout << "[Analytics] MeanResult -> Time: " << time
               << " | MeanOP: " << MeanOP
               << " | MeanHP: " << MeanHP
               << " | MeanLP: " << MeanLP
